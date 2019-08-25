880ae6949dac593802df2ca5ce2ebf6483953206
ericlewis <ericlewis777@gmail.com>
2019-08-24 22:53:53 -0400

    Initial Commit

--- Sources/ActivityIndicatorView/ActivityIndicatorView.swift
+++ Sources/ActivityIndicatorView/ActivityIndicatorView.swift
@@ -1,3 +1,24 @@
-struct ActivityIndicatorView {
-    var text = "Hello, World!"
+import SwiftUI
+
+struct ActivityIndicatorView: UIViewRepresentable {
+    @Binding var isAnimating: Bool
+    
+    private var style: UIActivityIndicatorView.Style?
+
+    init(isAnimating: Binding<Bool>, style: UIActivityIndicatorView.Style? = nil) {
+        _isAnimating = isAnimating
+        self.style = style
+    }
+    
+    func makeUIView(context: Context) -> UIActivityIndicatorView {
+        guard let style = self.style else {
+            return UIActivityIndicatorView()
+        }
+        
+        return UIActivityIndicatorView(style: style)
+    }
+    
+    func updateUIView(_ uiView: UIActivityIndicatorView, context: Context) {
+        isAnimating ? uiView.startAnimating() : uiView.stopAnimating()
+    }
 }
--- Tests/ActivityIndicatorViewTests/ActivityIndicatorViewTests.swift
+++ Tests/ActivityIndicatorViewTests/ActivityIndicatorViewTests.swift
@@ -3,10 +3,7 @@ import XCTest
 
 final class ActivityIndicatorViewTests: XCTestCase {
     func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(ActivityIndicatorView().text, "Hello, World!")
+        XCTAssertEqual(ActivityIndicatorView(isAnimating: .constant(true)).isAnimating, true)
     }
 
     static var allTests = [
