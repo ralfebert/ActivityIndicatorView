5b8a02ee75eae7806c615ea42941c412bcd87769
Ralf Ebert <info@ralfebert.de>
2020-05-17 21:12:35 +0200

    consistent code formatting, clear trailing whitespace

---
+++ Makefile
@@ -0,0 +1,2 @@
+format:
+	swiftformat --swiftversion 5.2 --indentcase true --stripunusedargs unnamed-only --self insert --disable blankLinesAtStartOfScope,blankLinesAtEndOfScope Sources Tests
--- Sources/ActivityIndicatorView/ActivityIndicatorView.swift
+++ Sources/ActivityIndicatorView/ActivityIndicatorView.swift
@@ -3,7 +3,7 @@ import SwiftUI
 public struct ActivityIndicatorView: UIViewRepresentable {
     @Binding var isAnimating: Bool
     var color: UIColor
-    
+
     private var style: UIActivityIndicatorView.Style?
 
     public init(isAnimating: Binding<Bool>, style: UIActivityIndicatorView.Style? = nil, color: UIColor = .systemGray) {
@@ -11,18 +11,18 @@ public struct ActivityIndicatorView: UIViewRepresentable {
         self.style = style
         self.color = color
     }
-    
+
     public func makeUIView(context: Context) -> UIActivityIndicatorView {
         guard let style = self.style else {
             return UIActivityIndicatorView()
         }
-        
+
         return UIActivityIndicatorView(style: style)
     }
-    
+
     public func updateUIView(_ uiView: UIActivityIndicatorView, context: Context) {
-        uiView.color = color
-        isAnimating ? uiView.startAnimating() : uiView.stopAnimating()
+        uiView.color = self.color
+        self.isAnimating ? uiView.startAnimating() : uiView.stopAnimating()
     }
 }
 
