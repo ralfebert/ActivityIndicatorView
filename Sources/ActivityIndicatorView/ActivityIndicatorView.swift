56322e238f2e2446ddc4df645228ca98095b9f93
Ralf Ebert <info@ralfebert.de>
2020-05-17 21:16:53 +0200

    simplify implementation

--- Sources/ActivityIndicatorView/ActivityIndicatorView.swift
+++ Sources/ActivityIndicatorView/ActivityIndicatorView.swift
@@ -3,31 +3,20 @@ import SwiftUI
 public struct ActivityIndicatorView: UIViewRepresentable {
     @Binding var isAnimating: Bool
     var color: UIColor
+    var style: UIActivityIndicatorView.Style
 
-    private var style: UIActivityIndicatorView.Style?
-
-    public init(isAnimating: Binding<Bool>, style: UIActivityIndicatorView.Style? = nil, color: UIColor = .systemGray) {
+    public init(isAnimating: Binding<Bool> = .constant(true), style: UIActivityIndicatorView.Style = .medium, color: UIColor = .systemGray) {
         _isAnimating = isAnimating
         self.style = style
         self.color = color
     }
 
     public func makeUIView(context: Context) -> UIActivityIndicatorView {
-        guard let style = self.style else {
-            return UIActivityIndicatorView()
-        }
-
-        return UIActivityIndicatorView(style: style)
+        UIActivityIndicatorView(style: style)
     }
 
     public func updateUIView(_ uiView: UIActivityIndicatorView, context: Context) {
         uiView.color = self.color
         self.isAnimating ? uiView.startAnimating() : uiView.stopAnimating()
-    }
-}
-
-public extension ActivityIndicatorView {
-    init(style: UIActivityIndicatorView.Style? = nil, color: UIColor = .systemGray) {
-        self.init(isAnimating: .constant(true), style: style, color: color)
     }
 }
